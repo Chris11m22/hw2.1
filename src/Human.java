280d6d70bf19e014e4d8f9752b319755499ed211
christinam <malininak11@gmail.com>
2022-11-18 20:15:29 +0500

    1

--- src/Human.java
+++ src/Human.java
@@ -1,41 +1,57 @@
 public class Human {
-     int yearOfBirth;
-    String name;
-     String town;
-     String jobTitle;
+    private int yearOfBirth;
+    private String town;
+    public String name;
+    public String jobTitle;
 
 
-    Human (String name, String town, String jobTitle, int yearOfBirth) {
-        this.name = name;
+    Human(String name, String town, String jobTitle, int yearOfBirth) {
         if (name == null) {
-            name = "Информация не указана";
-        }
-        this.name = name;
-
-        this.town = town;
-        if (town == null) {
-            town = "Информация не указана";
-        }
-        this.town = town;
-
-
-        this.jobTitle = jobTitle;
-        if (jobTitle == null) {
-            jobTitle = "Информация не указана";
-        }
-        this.jobTitle = jobTitle;
-
-        if (yearOfBirth >= 0) {
-            this.yearOfBirth = yearOfBirth;
+           this.name = "Информация не указана";
         } else {
-            this.yearOfBirth = Math.abs(yearOfBirth);
+            this.name = name;
+        }
+        if (town == null) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (jobTitle == null) {
+            this.jobTitle = "Информация не указана";
+        } else {
+            this.jobTitle = jobTitle;
+        }
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
         }
 
     }
 
-    @Override
-    public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " +yearOfBirth + " году." + " Я работаю на должности "+ jobTitle +". Будем знакомы!";
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        if (town == null) {
+            town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+
     }
 }
 
